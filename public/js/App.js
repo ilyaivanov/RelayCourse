a00ded2c163006c72189423369f6bb5421b94291
Ilya Ivanov <static.ila@gmail.com>
2016-10-05 19:19:19 +0300

    Remove slice and prop validation

--- public/js/App.js
+++ public/js/App.js
@@ -3,18 +3,10 @@ import Relay from 'react-relay';
 import Link from './Components/link';
 
 class App extends React.Component {
-    static propTypes = {
-        limit: PropTypes.number.isRequired,
-        store: PropTypes.object.isRequired,
-    };
-    static defaultProps = {
-        limit: 10
-    };
-
     render() {
         let {store, limit} = this.props;
 
-        let content = store.links.slice(0, limit).map(l => (<Link key={l._id} link={l}/>));
+        let content = store.links.map(l => (<Link key={l._id} link={l}/>));
 
         return (
             <div>

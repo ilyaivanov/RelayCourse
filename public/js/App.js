56d42bb47a5b45e7e5fe8148b3e48f51be972413
Ilya Ivanov <static.ila@gmail.com>
2016-10-05 18:38:47 +0300

    Refactor to ES6 class

--- public/js/App.js
+++ public/js/App.js
@@ -1,18 +1,25 @@
 import React, {PropTypes} from 'react';
 
-let App = props => <div>
-    <ul>
-        {props.links.slice(0,props.limit).map(l => <li key={l._id}><a href={l.url}>{l.title}</a></li>)}
-    </ul>
-    <div>Showing: 10 items per page</div>
-    <div>Page: 1/2</div>
-    <button>prev</button>
-    <button>next</button>
-</div>;
+class App extends React.Component {
+    static propTypes = {
+        limit: PropTypes.number.isRequired
+    };
 
-App.propTypes = {
-    limit : PropTypes.number.isRequired
-};
+    render() {
+        let {links, limit} = this.props;
+        return (
+            <div>
+                <ul>
+                    {links.slice(0, limit).map(l => <li key={l._id}><a href={l.url}>{l.title}</a></li>)}
+                </ul>
+                <div>Showing: 10 items per page</div>
+                <div>Page: 1/2</div>
+                <button>prev</button>
+                <button>next</button>
+            </div>
+        );
+    }
+}
 
 export default App;
 

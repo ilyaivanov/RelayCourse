d3537a392f01495928d1840209d8a85451ec8490
Ilya Ivanov <static.ila@gmail.com>
2016-10-05 18:47:40 +0300

    Add store as root to schema

--- data/schema.js
+++ data/schema.js
@@ -5,27 +5,38 @@ import {
     GraphQLList,
     GraphQLString
 } from 'graphql';
-
-let linkType = new GraphQLObjectType({
-    name: 'Link',
-    fields: () => ({
-        _id: {type: GraphQLString},
-        title: {type: GraphQLString},
-        url: {type: GraphQLString}
-    })
-});
-
-let Schema = db => new GraphQLSchema({
-    query: new GraphQLObjectType({
-        name: 'query',
+let store = {};
+let Schema = function (db) {
+    let storeType = new GraphQLObjectType({
+        name: 'Store',
         fields: ()=> ({
             links: {
                 type: new GraphQLList(linkType),
                 resolve: () => db.collection('links').find({}).toArray()
             }
         })
-    })
-    // mutation:...
-});
+    });
+
+    let linkType = new GraphQLObjectType({
+        name: 'Link',
+        fields: () => ({
+            _id: {type: GraphQLString},
+            title: {type: GraphQLString},
+            url: {type: GraphQLString}
+        })
+    });
+
+    return new GraphQLSchema({
+        query: new GraphQLObjectType({
+            name: 'Query',
+            fields: ()=> ({
+                store: {
+                    type: storeType,
+                    resolve: () => store
+                }
+            })
+        })
+    });
+};
 
 export default Schema;
